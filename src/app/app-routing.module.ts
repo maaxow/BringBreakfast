c751f130f825dc9dcef87863f474759313784fca
Max <maxime.rose@hotmail.fr>
2019-10-01 01:11:44 +0200

    adding pages team and member

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { MemberComponent } from './member/member.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'member', component: MemberComponent },
 ];
 
 @NgModule({
