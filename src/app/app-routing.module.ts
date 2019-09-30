511549d11cc515a2fa2404582be2d9719853065a
Max <maxime.rose@hotmail.fr>
2019-10-01 01:18:30 +0200

    create longin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TeamComponent } from './team/team.component';
 import { MemberComponent } from './member/member.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'team', component: TeamComponent },
   { path: 'member', component: MemberComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
