08f5a600a4ff8be4dcf98a9af77461455ac0371e
Themistoklis <themis.bo@gmail.com>
2022-10-05 13:15:00 +0300

    Migrate to 11

--- front-end/src/app/member/member.component.spec.ts
+++ front-end/src/app/member/member.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MemberComponent } from './member.component';
 
@@ -6,7 +6,7 @@ describe('MemberComponent', () => {
   let component: MemberComponent;
   let fixture: ComponentFixture<MemberComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MemberComponent ]
     })
